ba4fa290c912ee0015ad712b0b5f01239d09c303
Fatima Ezzahra Houd <f.houd@etu.enset-media.ac.ma>
2023-10-30 09:28:30 +0100

    wallet repo & web

--- wallet-service/src/main/java/com/houd/walletservice/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/houd/walletservice/WalletServiceApplication.java
@@ -1,7 +1,13 @@
 package com.houd.walletservice;
 
+import com.houd.walletservice.repositories.WalletRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class WalletServiceApplication {
@@ -9,5 +15,6 @@ public class WalletServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WalletServiceApplication.class, args);
 	}
+	
 
 }
